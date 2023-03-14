86dd7019d7d830bc13787f8e83b77b1cd8ce0c49
mai.ngoc.tri <mai.ngoc.tri@sun-asterisk.com>
2023-03-14 13:25:30 +0700

    config vite react typescript

---
+++ vite.config.ts
@@ -0,0 +1,13 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react-swc'
+import eslint from 'vite-plugin-eslint'
+
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react(), eslint()],
+  server: {
+    port: 9000, // setting port
+    strictPort: true, // exit if port is already in use
+  }
+})
