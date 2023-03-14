8ef6693933d8bfb22c24961cfbbba43d0d7ab140
mai.ngoc.tri <mai.ngoc.tri@sun-asterisk.com>
2023-03-14 15:10:16 +0700

    config eslint and prettier

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,11 @@
-import { StrictMode } from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
 
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+import App from './App';
+import './index.css';
+
+createRoot(document.getElementById('root') as HTMLElement).render(
   <StrictMode>
     <App />
   </StrictMode>,
-)
+);
--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,12 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
-import eslint from 'vite-plugin-eslint'
-
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react-swc';
+import eslint from 'vite-plugin-eslint';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), eslint()],
-  server: {
-    port: 9000, // setting port
-    strictPort: true, // exit if port is already in use
-  }
-})
+	plugins: [react(), eslint()],
+	server: {
+		port: 9000, // setting port
+		strictPort: true, // exit if port is already in use
+	},
+});
